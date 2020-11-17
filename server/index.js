fde13edec2a072f331a76b705033820d3de62cee
perezsergioace <perez.sergio.ace@gmail.com>
2020-11-16 22:47:42 -0500

    added new route: can now update a specific todo and delete a specific todo

--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 // routes
 
 // create a todo
-
 app.post("/todos", async (req, res) => {
     try {
         const { description } = req.body;
@@ -44,7 +43,31 @@ app.get("/todos/:id", async (req, res) => {
     }
 });
 
+// update a todo
+app.put("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+
+        res.json("Todo was updated.")
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
 // delete a todo
+app.delete("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+
+        res.json("Todo was deleted.")
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 app.listen(5000, () => {
     console.log("Server has started on port 5000.");
