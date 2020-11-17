6a625d96d53c6c2f8760ffbc968c04bd19d90d1d
perezsergioace <perez.sergio.ace@gmail.com>
2020-11-16 23:07:17 -0500

    Cleaned up the inex.js file

--- server/index.js
+++ server/index.js
@@ -9,29 +9,31 @@ app.use(express.json());
 
 // routes
 
-// create a todo
+// Creat a todo
 app.post("/todos", async (req, res) => {
     try {
         const { description } = req.body;
         const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
+
         res.json(newTodo.rows[0]);
     } catch (err) {
         console.error(err.message);
     }
 });
 
-// get all todos
+// Get all todos
 app.get("/todos", async (req, res) => {
     try {
         const allTodos = await pool.query("SELECT * FROM todo");
+
         res.json(allTodos.rows);
     } catch (err) {
         console.error(err.message);
-        
+
     }
 });
 
-// get a todo
+// Get a todo
 app.get("/todos/:id", async (req, res) => {
     try {
         const { id } = req.params;
@@ -43,12 +45,11 @@ app.get("/todos/:id", async (req, res) => {
     }
 });
 
-// update a todo
+// Update a todo
 app.put("/todos/:id", async (req, res) => {
     try {
         const { id } = req.params;
         const { description } = req.body;
-
         const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
 
         res.json("Todo was updated.")
@@ -57,7 +58,7 @@ app.put("/todos/:id", async (req, res) => {
     }
 })
 
-// delete a todo
+// Delete a todo
 app.delete("/todos/:id", async (req, res) => {
     try {
         const { id } = req.params;
