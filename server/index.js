821d709e91adc7d3a4f605b8cef5194336403469
perezsergioace <perez.sergio.ace@gmail.com>
2020-11-16 22:34:33 -0500

    added new route: can now get a specific todo from the database

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.post("/todos", async (req, res) => {
     } catch (err) {
         console.error(err.message);
     }
-})
+});
 
 // get all todos
 app.get("/todos", async (req, res) => {
@@ -30,9 +30,19 @@ app.get("/todos", async (req, res) => {
         console.error(err.message);
         
     }
-})
+});
 
 // get a todo
+app.get("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+
+        res.json(todo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 // delete a todo
 
